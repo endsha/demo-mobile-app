4ef14b076c8ead999fa7a73fc1b34f5322f333d0
endsha <anh.ns.dev@gmail.com>
2022-04-21 11:51:44 +0700

    feat: install react-native-vector-icons library on ios

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { CALENDAR } from '@constants/routes';
 import NavigationService from '@utils/navigationService';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Dashboard = () => {
   const navigateToCalender = () => {
@@ -13,6 +14,7 @@ const Dashboard = () => {
       <Text>DASHBOARD SCREEN</Text>
       <TouchableOpacity onPress={() => navigateToCalender()}>
         <Text>Navigate To Calendar</Text>
+        <Icon name='cancel' size={24}/>
       </TouchableOpacity>
     </SafeAreaView>
   );
