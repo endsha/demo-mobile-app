7f4ecc17c3803684cdf70efc9a0152ffa899eeb4
endsha <anh.ns.dev@gmail.com>
2022-04-26 15:01:03 +0700

    feat: add task overlapse feature

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   View,
   Text,
@@ -13,7 +13,7 @@ import Colors from '@constants/colors';
 
 const hours = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23,
+  22, 23, 24,
 ];
 
 const hourHeight = 52;
@@ -23,66 +23,70 @@ const Day = () => {
     <View style={styles.dayContainer}>
       {hours.map((hour, index) => (
         <View style={styles.hourContainer} key={`hour-${index}`}>
-          <Text>
-            {hour > 12 ? hour % 12 : hour} {hour > 12 ? 'PM' : 'AM'}
-          </Text>
+          {hour !== 24 ? (
+            <Text>
+              {hour > 12 ? hour % 12 : hour} {hour > 12 ? 'PM' : 'AM'}
+            </Text>
+          ) : (
+            <Text>0 AM</Text>
+          )}
         </View>
       ))}
-      <View style={styles.hourContainer} key={`hour-24`}>
-        <Text>0 AM</Text>
-      </View>
     </View>
   );
 };
 
 const Tasks = props => {
+  const sortedTasks = props.tasks.sort((prev, next) => prev.start - next.start);
+
   return (
-    <View style={{ paddingRight: 20 }}>
-      {hours.map(hour => {
-        const hourTasks = [];
-        props.tasks.map(task => {
-          if (hour === task.start) {
-            hourTasks.push({ ...task, isStart: true });
-          } else if (hour === task.end) {
-            hourTasks.push({ ...task, isEnd: true });
-          } else if (task.start < hour && task.end > hour) {
-            hourTasks.push({ ...task, isMiddle: true });
-          }
-        });
-        if (hourTasks.length === 0) {
-          return (
-            <View style={styles.emptyTask}>
-              <View style={styles.dashLine} />
+    <View style={{ width: '100%' }}>
+      {hours.map((hour, hourIndex) => {
+        const emptyHourIndex = props.tasks.findIndex(
+          task => task.start <= hour && task.end >= hour,
+        );
+        return (
+          <View style={styles.emptyTask} key={`hour-${hourIndex}`}>
+            {emptyHourIndex === -1 && <View style={styles.dashLine} />}
+          </View>
+        );
+      })}
+      {sortedTasks.map((task, taskIndex) => {
+        const taskHeight = hourHeight * (task.end - task.start + 1);
+        return (
+          <View
+            style={{
+              ...styles.taskContainer,
+              top: hourHeight * task.start,
+              height: taskHeight,
+            }}
+            key={`task-${taskIndex}`}
+          >
+            <View
+              style={{
+                ...styles.taskTime,
+                backgroundColor: task.color,
+                height: taskHeight - hourHeight / 2 - 16,
+              }}
+            >
+              <Text style={styles.taskNameText}>{task.name}</Text>
             </View>
-          );
-        } else {
-          if (hourTasks.length === 1) {
-            return (
-              <View
-                style={{
-                  ...styles.taskContainer,
-                  height: hourHeight,
-                }}
-              >
-                <View
-                  style={{
-                    ...styles.taskTime,
-                    height: hourTasks[0].isEnd ? hourHeight / 2 - 10 : hourHeight,
-                  }}
-                />
-                {hourTasks[0].isStart && <Text>{hourTasks[0].name}</Text>}
-              </View>
-            );
-          }
-        }
+          </View>
+        );
       })}
     </View>
   );
 };
 
 const CalendarScreen = () => {
+  const agendaRef = useRef(null);
+
   const goBackToHome = () => {
     NavigationService.goBack();
+  };
+
+  const openCalendar = () => {
+    agendaRef.current.chooseDay(new Date(), true);
   };
 
   return (
@@ -101,11 +105,17 @@ const CalendarScreen = () => {
       </View>
       <Text style={styles.subHeading}>Productive Day, Phillip</Text>
       <View style={{ flexDirection: 'row' }}>
-        <TouchableOpacity style={styles.monthBtn}>
+        <TouchableOpacity
+          style={styles.monthBtn}
+          onPress={() => {
+            openCalendar();
+          }}
+        >
           <Text style={styles.monthBtnText}>April, 2022</Text>
         </TouchableOpacity>
       </View>
       <Agenda
+        ref={agendaRef}
         items={{
           '2022-04-22': [
             {
@@ -114,19 +124,34 @@ const CalendarScreen = () => {
                   name: 'Testing',
                   start: 0,
                   end: 6,
+                  color: '#FBE3CA',
+                },
+                {
+                  name: 'Testing',
+                  start: 2,
+                  end: 4,
+                  color: '#DBE4DD',
+                },
+                {
+                  name: 'Testing',
+                  start: 8,
+                  end: 10,
+                  color: '#F9D6D7',
                 },
               ],
             },
           ],
         }}
         selected={'2022-04-22'}
-        hideKnob
-        renderEmptyData={() => <Day />}
+        renderEmptyData={() => (
+          <View style={styles.emptyData}>
+            <Text>This day have no task.</Text>
+          </View>
+        )}
         renderItem={item => <Tasks tasks={item.tasks} />}
         renderDay={() => {
           return <Day />;
         }}
-        disabledDaysIndexes={[0, 6]}
         theme={{
           dotColor: 'transparent',
           selectedDotColor: 'transparent',
@@ -134,9 +159,7 @@ const CalendarScreen = () => {
           calendarBackground: Colors.backgroundColor,
           selectedDayBackgroundColor: Colors.backgroundColor,
           selectedDayTextColor: '#D46B74',
-          // textSectionTitleColor: '#D46B74',
           textSectionTitleDisabledColor: 'red',
-          monthTextColor: 'blue',
         }}
       />
     </SafeAreaView>
@@ -186,6 +209,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '500',
   },
+  emptyData: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   dayContainer: {
     flexDirection: 'column',
     paddingHorizontal: 24,
@@ -198,6 +225,7 @@ const styles = StyleSheet.create({
   },
   emptyTask: {
     height: hourHeight,
+    paddingRight: 20,
   },
   dashLine: {
     width: '100%',
@@ -208,12 +236,22 @@ const styles = StyleSheet.create({
     opacity: 0.1,
     marginTop: 8,
   },
-  taskContainer: {},
+  taskContainer: {
+    paddingRight: 20,
+    position: 'absolute',
+    left: 0,
+    width: '100%',
+  },
   taskTime: {
-    backgroundColor: '#FBE3CA',
     position: 'absolute',
     top: 0,
     left: 0,
     width: '100%',
+    borderRadius: 20,
+    padding: 20,
+  },
+  taskNameText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
